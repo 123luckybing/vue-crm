cb79e63c18edb5449c168aa92cca8284340a7fc5
周芳冰 <zhoufangbing@daojia-inc.com>
2019-04-14 14:42:09 +0800

    商品列表

---
+++ src/components/AddGoods.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-dialog title="添加商品" :visible.sync="visible">
+    <el-form :model="form" ref="form">
+      <el-form-item label="商品名称" :label-width="formLabelWidth" prop='name' :rules="[{ required: true, message: '请输入商品名称' }]">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="种类" :label-width="formLabelWidth" prop='kind' :rules="[{ required: true, message: '请输入种类' }]">
+        <el-select v-model="form.kind" placeholder="请选择种类">
+          <el-option v-for="(item, index) in kindList" :key="index" :label="item.name" :value="item.name" ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="价格" :label-width="formLabelWidth" prop='price' :rules="[{ required: true, message: '请输入价格价格' }]">
+          <el-input v-model="form.price" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="数量" :label-width="formLabelWidth" prop='number' :rules="[{ required: true, message: '请输入数量' }]">
+       <el-input v-model="form.number" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="简介" :label-width="formLabelWidth" prop='remark'>
+        <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="简介"
+          v-model="form.remark">
+        </el-input>
+      </el-form-item>
+      <!-- <el-form-item label="图片" :label-width="formLabelWidth" prop='image'>
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          :on-exceed="handleExceed"
+          :file-list="fileList"
+        >
+          <el-button size="small" type="primary">点击上传</el-button>
+        </el-upload>
+      </el-form-item> -->
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="cancel">取 消</el-button>
+      <el-button type="primary" @click="submit">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props:["visible"],
+  data() {
+    return {
+      kindList:[],
+      form: {
+        name: '',
+        price: '',
+        number: '',
+        kind: '',
+        remark: ''
+      },
+      formLabelWidth: '100px'
+    }
+  },
+  methods: {
+    // 关闭弹窗
+    cancel (){
+      this.$emit('alertCancel') // 调用父组件的方法
+    },
+    // 确定提交
+    submit () {
+      this.$refs["form"].validate((valid) => {
+          if (valid) {
+            const data = JSON.stringify(this.form)
+            axios.post('/goods/add', data).then((res) => {
+              this.$emit("alertCancel")
+              this.$refs['form'].resetFields()
+              this.$message({
+                message: res.data.msg,
+                type: 'success',
+                onClose: () => {
+                  this.$emit('getList')
+                }
+              })
+            }).catch((err) => {
+              console.log(err)
+            })
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    }
+  },
+  beforeCreate() {
+    axios.post('/kind/list',{}).then((res) => {
+      this.kindList = res.data.list
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/EditGoods.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-dialog title="修改商品" :visible.sync="visible">
+    <el-form :model="form" ref="form" v-if="form.id">
+       <el-form-item label="id:" :label-width="formLabelWidth" prop="id">
+        <div>{{ form.id }}</div>
+      </el-form-item>
+      <el-form-item label="名字:" :label-width="formLabelWidth" prop="name">
+        <div>{{ form.name }}</div>
+      </el-form-item>
+      <el-form-item label="种类:" :label-width="formLabelWidth" prop="kind">
+        <div>{{ form.kind }}</div>
+      </el-form-item>
+      <el-form-item label="库存:" :label-width="formLabelWidth" prop="number">
+        <el-input
+          placeholder="请输入内容"
+          v-model="form.number"
+         >
+        </el-input>
+      </el-form-item>
+      <el-form-item label="价格:" :label-width="formLabelWidth" prop="price">
+        <el-input
+          placeholder="请输入内容"
+          v-model="form.price"
+         >
+        </el-input>
+      </el-form-item>
+        <el-form-item label="简介:" :label-width="formLabelWidth" prop="remark">
+          <el-input
+            placeholder="请输入内容"
+            v-model="form.remark"
+          >
+          </el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="onSubmit">确认修改</el-button>
+      <el-button @click="cancelEdit">取消</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: ["info","visible"],
+  data() {
+    return {
+      form: {},
+      formLabelWidth: '100px'
+    }
+  },
+  // 父组件通过调用接口将获得的数据通过props传给子组件，
+  // 但是是异步的,所以updated再赋值一下
+  updated() {
+    this.form = this.info
+  },
+  methods: {
+    // 取消编辑弹框
+    cancelEdit () {
+      this.$emit("closeEdit")
+    },
+    // 修改
+    onSubmit () {
+      this.$refs["form"].validate((valid) => {
+        if(valid) {
+          const data = JSON.stringify(this.form)
+          this.$emit("closeEdit")
+          this.$refs['form'].resetFields()
+          axios.post('/goods/update', data ).then((res) => {
+            if(res.data.code === 0) {
+              this.$message({
+                message: "修改成功",
+                  type: 'success',
+                  onClose: () => {
+                    this.$emit('getList')
+                  }
+                })
+            } else {
+              this.$message({
+                message: "修改失败",
+                  type: 'error'
+              })
+            }
+          }).catch((err) => {
+            console.log(err)
+          })
+          console.log(this.form)
+        }
+      })
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
