fe5d5a2c9c8ae294a105cfb87711a5284ca2acd6
周芳冰 <zhoufangbing@daojia-inc.com>
2019-04-14 14:41:40 +0800

    商品列表

--- src/components/GoodList.vue
+++ src/components/GoodList.vue
@@ -1,58 +1,144 @@
 <template >
-  <el-table
-    :data="dataList"
-    border
-    class='table'
-  >
-    <el-table-column
-      prop="id"
-      label="序号"
+  <div>
+    <el-button
+      class='btn'
+      type="primary"
+      @click="alertShow"
+    >增加</el-button>
+    <el-table
+      :data="dataList"
+      border
+      class='table'
     >
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="商品名称"
-    >
-    </el-table-column>
-    <el-table-column
-      prop="price"
-      label="单价">
-    </el-table-column>
-    <el-table-column
-      prop="number"
-      label="库存">
-    </el-table-column>
-    <el-table-column
-      prop="kind"
-      label="种类">
-    </el-table-column>
-    <el-table-column
-      prop="remark"
-      label="简介">
-    </el-table-column>
-    <el-table-column
-      prop="createtime"
-      label="创建时间">
-    </el-table-column>
-  </el-table>
+      <el-table-column
+        prop="id"
+        label="序号"
+        width="50"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="商品名称"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="单价"
+        width='70'
+      >
+      </el-table-column>
+      <el-table-column
+        prop="number"
+        label="库存"
+        width="70">
+      </el-table-column>
+      <el-table-column
+        prop="kind"
+        label="种类">
+      </el-table-column>
+      <el-table-column
+        prop="remark"
+        label="简介">
+      </el-table-column>
+      <el-table-column
+        prop="createTime"
+        label="创建时间"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+      label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button
+            @click="edit(scope.row.id)"
+            type="text"
+            size="small">
+            修改
+          </el-button>
+          <el-button
+            @click="deleteRow(scope.row.id)"
+            type="text"
+            size="small">
+            删除
+          </el-button>
+      </template>
+      </el-table-column>
+    </el-table>
+    <AddGoods :visible="addVisible" @alertCancel="alertCancel" @getList="getList"/>
+    <EditGoods :visible="editVisible" @closeEdit="closeEdit" @getList="getList" :info="detailInfo" />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import AddGoods from './AddGoods'
+import EditGoods from './EditGoods'
 export default {
+  components: {
+    AddGoods,
+    EditGoods
+  },
   data () {
     return {
-      dataList: []
+      dataList: [],
+      addVisible: false,
+      detailInfo: {},
+      editVisible: false
     }
   },
-  beforeCreate () {
-    const data = JSON.stringify({name: '', kind: ''})
-    axios.post('/goods/list', data).then((res) => {
-      this.dataList = res.data.data
-      console.log(res)
-    }).catch((err) => {
-      console.log(err)
-    })
+  methods: {
+    // 编辑弹框消失
+    closeEdit () {
+      this.editVisible = false
+    },
+    getList () {
+      const data = JSON.stringify({name: '', kind: ''})
+      axios.post('/goods/list', data).then((res) => {
+        this.dataList = res.data.list
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    // 修改
+    edit(id) {
+      axios.get('/goods/get', {
+        params: {
+          id: id
+        }
+      }).then((res) => {
+        this.detailInfo = res.data
+        this.editVisible = true
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    // 增加弹框消失
+    alertCancel () {
+      this.addVisible = false
+    },
+    // 增加弹框出现
+    alertShow() {
+      this.addVisible = true
+    },
+    // 删除
+    deleteRow (id) {
+      const data = JSON.stringify({id: id})
+      axios.post('/goods/del', data).then((res) => {
+        this.$message({
+          message: res.data.msg,
+          type: 'success',
+          onClose: () => {
+            this.getList()
+          }
+        })
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+  mounted () {
+    this.getList()
   }
 }
 </script>
@@ -64,5 +150,9 @@ export default {
 
 .el-table {
   width: auto;
+}
+
+.btn {
+  margin: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import {
   Pagination,
   Dialog,
@@ -148,6 +149,9 @@ Vue.prototype.$notify = Notification
 Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://172.20.10.8:8080' // 设置一个类似base_url的请求路径
+global.axios = axios // 设置一个全局axios便于调用
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 /* eslint-disable no-new */
 new Vue({
