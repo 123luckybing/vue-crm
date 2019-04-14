afd5888eb419e6f9bf4267d0991d7712b4271024
周芳冰 <zhoufangbing@daojia-inc.com>
2019-04-14 22:46:58 +0800

    订单列表

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>Orderlist</div>
+  <el-table
+      :data="dataList"
+      border
+      class='table'
+    >
+      <el-table-column
+        prop="id"
+        label="序号"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="goodsName"
+        label="商品名称"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="goodsKind"
+        label="商品种类">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="订单状态">
+      </el-table-column>
+      <el-table-column
+        prop="number"
+        label="商品数量">
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="商品单价">
+      </el-table-column>
+      <el-table-column
+        prop="priceTotal"
+        label="商品总价">
+      </el-table-column>
+      <el-table-column
+        prop="createTime"
+        label="创建时间">
+      </el-table-column>
+      <el-table-column
+      label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button
+            @click="del(scope.row.id)"
+            type="text"
+            size="small">
+            删除
+          </el-button>
+      </template>
+      </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      dataList: []
+    }
+  },
+  methods: {
+    // 请求列表
+    getList() {
+      axios.get('/order/list').then((res) => {
+        this.dataList = res.data
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    // 删除
+    del(id) {
+      const data = JSON.stringify({id: id})
+      axios.post('/order/del', data).then((res) => {
+        if(res.data.code === 0) {
+          this.$message({
+          message: res.data.msg,
+          type: 'success',
+          onClose: () => {
+            this.getList()
+          }
+        })
+        } else {
+          this.$message({
+          message: res.data.msg,
+          type: 'error'
+        })
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+  mounted() {
+    this.getList()
+  }
 }
 </script>
 
 <style scoped>
+.table {
+  margin: 10px;
+}
 
+.el-table {
+  width: auto;
+}
 </style>
